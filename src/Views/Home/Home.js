71aa4087fbde799f29171b9d6ffc2cf5c6fc8a00
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-29 17:49:17 -0700

    removing console logging error

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -4,10 +4,9 @@ import DogList from '../../Components/DogList/DogList';
 import { fetchDogs } from '../../services/dogs';
 
 export default function Home() {
-  // dogs object
   const [dogs, setDogs] = useState([]);
   const [loading, setLoading] = useState(true);
-  //useEffect hook to fetch dogs
+  const [error, setError] = useState('');
   useEffect(() => {
     try {
       const fetchData = async () => {
@@ -17,13 +16,13 @@ export default function Home() {
       };
       fetchData();
     } catch (e) {
-      console.error(e);
+      setError('there was an error');
     }
   }, []);
-  // console.log('dogs', dogs);
   if (loading) return (<h3>loading...</h3>);
   return (
     <div>
+      {error && <p>{error}</p>}
       <h1>Checkout Our Dogs</h1>
       <h3>Click an image to learn more about that dog.</h3>
       <DogList {...{ dogs }} />
