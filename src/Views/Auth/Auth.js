164e5b8f4359282865653c5ab8fa959f10fadee2
Mary Martinez <mary.diana.martinez@gmail.com>
2022-04-01 15:34:05 -0700

    fixing useState in Auth page

--- src/Views/Auth/Auth.js
+++ src/Views/Auth/Auth.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import AuthForm from '../../Components/Auth/AuthForm';
 import { signInUser } from '../../services/users';
 
